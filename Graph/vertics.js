08dd088af52a6b202c6a5cf503bcf40fff5673bc
Gyanendra Prakash <pgyanendra333@gmail.com>
2024-04-01 21:33:03 +0530

    first commit

---
+++ Graph/vertics.js
@@ -0,0 +1,23 @@
+
+// Prints the vertex and adjacency list
+printGraph()
+{
+    // get all the vertices
+    var get_keys = this.AdjList.keys();
+
+    // iterate over the vertices
+    for (var i of get_keys) {
+        // get the corresponding adjacency list
+        // for the vertex
+        var get_values = this.AdjList.get(i);
+        var conc = "";
+
+        // iterate over the adjacency list
+        // concatenate the values into a string
+        for (var j of get_values)
+            conc += j + " ";
+
+        // print the vertex and its adjacency list
+        console.log(i + " -> " + conc);
+    }
+}
