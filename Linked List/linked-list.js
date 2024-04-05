d1b2817cc084abab1802f4adc997e650e8f4873a
Gyanendra Prakash <pgyanendra333@gmail.com>
2024-04-05 16:06:26 +0530

    linked list

---
+++ Linked List/linked-list.js
@@ -0,0 +1,24 @@
+
+class List {
+    constructor(data) {
+        this.head = {
+            value: data,
+            next: null
+        }
+        this.tail = this.head;
+    }
+    appendNode(nodeData) {
+        let newNode = {
+            value: nodeData,
+            next: null
+        }
+        this.tail.next = newNode;
+        this.tail = newNode;
+    }
+}
+let list = new List(100);
+list.appendNode(300);
+list.appendNode(400);
+
+
+console.log(list);
