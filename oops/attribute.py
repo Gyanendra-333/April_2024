02b533d6fb411703ec7549bf96ac41c4928eeb1b
Gyanendra Prakash <pgyanendra333@gmail.com>
2024-04-08 10:21:22 +0530

    attribute

---
+++ oops/attribute.py
@@ -0,0 +1,18 @@
+
+class Human:
+
+    def __init__(self) -> None:
+        print("My Created..")
+
+    def eat(self):
+        print("Eating..")
+
+    def walk(self):
+        print("Walking...")
+
+
+gyan = Human()
+gyan.eat()
+
+hari = Human()
+hari.walk()
