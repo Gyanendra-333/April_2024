0326cbe91979f4221e951d9a2bfac40b09430a62
Gyanendra Prakash <pgyanendra333@gmail.com>
2024-04-08 10:21:30 +0530

    opps

---
+++ oops/oop3.py
@@ -0,0 +1,13 @@
+
+class Human:
+
+    def __init__(self, f, l, a):
+        self.first_name = f
+        self.last_name = l
+        self.age = a
+
+
+gyanendra = Human("Ram", "Shyma", 10000)
+gyanendra.skills = "Super Power"
+
+print(gyanendra.skills)
