7d4cef12129784776a15b0d0948073428ec63f8e
Gyanendra Prakash <pgyanendra333@gmail.com>
2024-04-08 10:21:38 +0530

    self

---
+++ oops/self-attribute.py
@@ -0,0 +1,15 @@
+
+class Human:
+
+    def __init__(self, f, l, a):
+        self.first_name = f
+        self.last_name = l
+        self.age = a
+
+        def full_name(self):
+            print(f"full name = {self.first_name}")
+
+
+gyan = Human("Gyanendra", "Insan", 27)
+
+gyan.full_name()
