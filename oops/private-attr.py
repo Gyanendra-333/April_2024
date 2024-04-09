ed3015a34eb875474604a334c93fef5a07e35eb2
Gyanendra Prakash <pgyanendra333@gmail.com>
2024-04-09 10:17:21 +0530

    private attribute

---
+++ oops/private-attr.py
@@ -0,0 +1,14 @@
+
+
+class Human:
+
+    def __init__(self, f, l, a):
+        self.first_name = f
+        self.last_name = l
+        self._age = a  # private Attribute
+
+        def full_name(self):
+            print(f"full name = {self.first_name}")
+
+
+gyan = Human("Gyanendra", "Insan", 27)
