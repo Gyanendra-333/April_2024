362fbfdb197ba080440117d0a2e2052656386f5f
Gyanendra Prakash <pgyanendra333@gmail.com>
2024-04-09 10:16:57 +0530

    inheritance

---
+++ oops/inheritance.py
@@ -0,0 +1,22 @@
+
+
+class Teacher:
+    def makeAttendence(self):
+        print("Attendence Done")
+
+    def leaveApplication(self):
+        print("Leave Apllicaton")
+
+    def uploadTest(self):
+        print("Upload Test Online")
+
+
+class Student:
+    def makeAttendence(self):
+        print("Attendence Done")
+
+    def leaveApplication(self):
+        print("Leave Apllicaton")
+
+    def uploadData(self):
+        print("Upload Data Online")
