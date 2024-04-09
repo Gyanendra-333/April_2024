76935130767cf90d2bb8f81c7b13217f9ed62421
Gyanendra Prakash <pgyanendra333@gmail.com>
2024-04-09 10:17:09 +0530

    magic method

---
+++ oops/magic-method.py
@@ -0,0 +1,11 @@
+
+# print(dir(int))    - All Magic Method command
+
+class Human:
+    def __init__(self, n, a):
+        self.name = n
+        self.age = a
+
+
+gyanendra = Human("Gyan", 27)
+print(gyanendra)
