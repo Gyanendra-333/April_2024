9d39f42e79cbf2bc2ae526be900c1a46e6555f97
Gyanendra Prakash <pgyanendra333@gmail.com>
2024-04-10 09:36:26 +0530

    mathematical opeation in python

---
+++ Inter/operation.py
@@ -0,0 +1,16 @@
+
+num1 = 5  # integer
+num2 = 9.5  # Float
+
+result1 = num1+num2
+result2 = num1-num2
+result3 = num1*num2
+result4 = num1/num2
+result5 = num1 % num2
+
+
+print(result1)
+print(result2)
+print(result3)
+print(result4)
+print(result5)
