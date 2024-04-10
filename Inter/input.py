3a11dbcb9e6107d5773e7ef62994d70578d33095
Gyanendra Prakash <pgyanendra333@gmail.com>
2024-04-10 09:36:05 +0530

    input in python

---
+++ Inter/input.py
@@ -0,0 +1,13 @@
+
+# print("Please Enter Your Name: ")
+# input()  # input function
+
+# Type conversion
+# intger
+# string
+# Boolean
+# Float
+
+data = 7
+data_type = type(data)
+print(data_type)
