fc012127b51c589937926c1833297b652adce86c
Gyanendra Prakash <pgyanendra333@gmail.com>
2024-04-10 09:38:55 +0530

    Data Type

--- Inter/input.py
+++ Inter/input.py
@@ -2,11 +2,11 @@
 # print("Please Enter Your Name: ")
 # input()  # input function
 
-# Type conversion
-# intger
-# string
-# Boolean
-# Float
+# Type conversion in Python Programming
+# intger   - Like 2,6,4,86
+# string   - Like "Ram","shyam"
+# Boolean  - Like True and False only
+# Float    - Like 2.3,45.56,65.45
 
 data = 7
 data_type = type(data)
