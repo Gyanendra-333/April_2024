6537262f5c91af7c705f9cb5c332eb17e0443262
Gyanendra Prakash <pgyanendra333@gmail.com>
2024-04-12 09:03:45 +0530

    if else statement

--- Js/if-else.js
+++ Js/if-else.js
@@ -8,6 +8,9 @@ if (condition) {
 else if (condition2) {
     // write condition2 if first condition is false
 }
+else if (condition3) {
+    // Write condition3 here 
+}
 else {
     // write if condition is false 
 }
