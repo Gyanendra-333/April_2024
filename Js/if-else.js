7c78b15bce904b9baf0f33722c59408c1fa17997
Gyanendra Prakash <pgyanendra333@gmail.com>
2024-04-12 09:02:38 +0530

    if else statement

---
+++ Js/if-else.js
@@ -0,0 +1,13 @@
+
+// If else condition in Javascript
+
+if (condition) {
+    // write condition here
+
+}
+else if (condition2) {
+    // write condition2 if first condition is false
+}
+else {
+    // write if condition is false 
+}
