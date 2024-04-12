6d209d12152ca383d7a216388fc3ce2256444862
Gyanendra Prakash <pgyanendra333@gmail.com>
2024-04-12 09:02:19 +0530

    function

---
+++ Js/function.js
@@ -0,0 +1,15 @@
+
+function nameOfFunction() {
+    // Write Logic here 
+}
+
+nameOfFunction();
+
+
+// innerHtml
+
+document.getElementById("id-name").innerHTML = "Hello World";
+
+// print anything in javaScript
+
+console.log("Hello World")
