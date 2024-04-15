9ce4538a9786d77dc2b7c8a6d754c3b3d8615c51
Gyanendra Prakash <pgyanendra333@gmail.com>
2024-04-15 15:39:29 +0530

    loop

---
+++ Js/for-loop.js
@@ -0,0 +1,20 @@
+
+// for loop 
+
+for (initilizaton - cond; end - cond; increment - decreement) {
+    //  write code here    
+}
+
+// while loop
+
+while (condition) {
+    // write code here 
+    increment - decreement
+}
+
+// Do-while loop
+
+do {
+    // write code here 
+    increment - decreement
+} while (condition);
