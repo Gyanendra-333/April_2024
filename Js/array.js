6380757cc1dc816e1660ec89d3735ec695f6d121
Gyanendra Prakash <pgyanendra333@gmail.com>
2024-04-15 15:39:22 +0530

    array

---
+++ Js/array.js
@@ -0,0 +1,15 @@
+
+let fruit1 = ["Apple", "Banana", "Watermelon", "Mango"];
+let fruits2 = ["Apple", "Banana", "Watermelon", "Mango"];
+
+
+// Array Method
+
+concat()
+indexOf()
+join()
+pop()
+
+reverse()
+toString()
+valueOf()
