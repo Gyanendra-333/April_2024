901bd949674cf8c509cbcda7efa5c618faf2b62a
Gyanendra Prakash <pgyanendra333@gmail.com>
2024-04-16 14:50:32 +0530

    switch case

---
+++ Js/switch.js
@@ -0,0 +1,14 @@
+
+switch (expression) {
+    case x:
+        // write code here 
+        break
+
+    case y:
+        // write case 2 here
+        break;
+
+    default:
+    // write default code here 
+
+}
