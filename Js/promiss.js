a02932e7f1955987699a6151d98be7314436eebd
Gyanendra Prakash <pgyanendra333@gmail.com>
2024-04-17 11:55:16 +0530

    proisses

---
+++ Js/promiss.js
@@ -0,0 +1,21 @@
+
+try {
+    // Block of code to try 
+}
+catch (err) {
+    // block of code to handle errors
+}
+
+
+// Window methods
+
+alert()
+blur()
+
+setInterval(() => {
+    // code write here
+}, 1000);
+
+setTimeout(() => {
+    // code write here
+}, 1000);
