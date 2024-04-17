8fd2e81aab97a17d7218e6614ddd5a1ae2fab364
Gyanendra Prakash <pgyanendra333@gmail.com>
2024-04-17 11:55:07 +0530

    events

---
+++ Js/event.js
@@ -0,0 +1,19 @@
+
+element.addeventlistner("click", () => {
+    //    write code here  
+});
+
+
+element.addeventlistner("contextMenu", () => {
+    //    write code here  
+});
+
+
+element.addeventlistner("doubleclick", () => {
+    //    write code here  
+});
+
+
+element.addeventlistner("mouseclick", () => {
+    //    write code here  
+});
